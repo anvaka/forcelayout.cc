200bb7100dc1101095d3ea41e340a44a009d3a83
anvaka <anvaka@gmail.com>
2016-06-12 15:14:15 -0700

    Layout is now a template. Can specify dimension as an argument

--- include/forcelayout.cc/layout.h
+++ include/forcelayout.cc/layout.h
@@ -12,6 +12,7 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <numeric>
 
 #include "nangraph.cc/graph.h"
 #include "random.cc/random.h"
@@ -50,28 +51,145 @@ struct LayoutSettings {
   }
 };
 
-
+template <size_t N>
 class ForceLayout {
   Graph &graph;
   LayoutSettings _settings;
   Random random;
-  std::vector<Body*> _bodies;
+  std::vector<Body<N>*> _bodies;
   // this maps nodeId to Body address. TODO: Find a way to not use this.
-  std::unordered_map<std::size_t, Body*>  _nodeIdToBody;
+  std::unordered_map<std::size_t, Body<N>*> _nodeIdToBody;
 
-  QuadTree tree;
+  QuadTree<N> tree;
 
-  void accumulate();
-  double integrate();
-  void updateDragForce(Body *body);
-  void updateSpringForce(Body *source);
-  void initBodies();
-  void setInitialPositions();
+  void accumulate() {
+    tree.insertBodies(_bodies);
+    auto bodiesCount = _bodies.size();
+
+#pragma omp parallel for
+    for (std::size_t i = 0; i < bodiesCount; ++i) {
+      auto body = _bodies[i];
+      body->force.reset();
+
+      tree.updateBodyForce(body);
+      updateDragForce(body);
+    }
+
+#pragma omp parallel for
+    for (std::size_t i = 0; i < bodiesCount; ++i) {
+      auto body = _bodies[i];
+      updateSpringForce(body);
+    }
+  }
+
+  double integrate() {
+    double timeStep = _settings.timeStep;
+    double totalV = 0;
+    auto bodiesCount = _bodies.size();
+
+#pragma omp parallel for
+    for (std::size_t i = 0; i < bodiesCount; ++i) {
+      auto body = _bodies[i];
+      double coeff = timeStep / body->mass;
+
+      body->velocity.addScaledVector(body->force, coeff);
+
+      double v = body->velocity.length();
+      totalV += v;
+
+      if (v > 1) body->velocity.normalize();
+
+      body->pos.addScaledVector(body->velocity, timeStep);
+    }
+    
+    return totalV/_bodies.size();
+
+  }
+  void updateDragForce(Body<N> *body) {
+    body->force.addScaledVector(body->velocity, -_settings.dragCoeff);
+  }
+  void updateSpringForce(Body<N> *source) {
+    Body<N> *body1 = source;
+
+    for (auto body2 : source->springs) {
+      Vector3<N> dist = body2->pos - body1->pos;
+      double r = dist.length();
+
+      if (r == 0) {
+        for (size_t i = 0; i < N; ++i) {
+          dist.coord[i] = (random.nextDouble() - 0.5) / 50;
+        }
+        r = dist.length();
+      }
+
+      double coeff = _settings.springCoeff * (r - _settings.springLength) / r;
+
+      body1->force.addScaledVector(dist, coeff);
+      body2->force.addScaledVector(dist, -coeff);
+    }
+
+  }
+  void initBodies() {
+    _bodies.reserve(graph.getNodesCount());
+
+    NodeCallback initBody = [&](const std::size_t& nodeId) -> bool {
+      auto body = new Body<N>();
+      auto node = graph.getNode(nodeId);
+      auto degree = node->degree();
+      body->mass = 1 + degree/3.0;
+      _bodies.push_back(body);
+      _nodeIdToBody[nodeId] = body; // TODO: Find a way to remove this and save ram.
+      return false;
+    };
+
+    graph.forEachNode(initBody);
+
+    // Now that we have bodies, let's add links:
+    LinkCallback initSpring = [&](const std::size_t& fromId, const std::size_t& toId, const std::size_t& linkId) -> bool {
+      // TODO: Add verification
+      _nodeIdToBody[fromId]->springs.push_back(_nodeIdToBody[toId]);
+      return false;
+    };
+    graph.forEachLink(initSpring);
+    setInitialPositions();
+  }
+
+  void setInitialPositions() {
+    // TODO: I think this can be done better. For example, combine
+    // CW algorithm with initial placement?
+    auto nodesCount = _bodies.size();
+
+    std::vector<int> degree(nodesCount);
+    std::iota(degree.begin(), degree.end(), 0);
+    std::sort(degree.begin(), degree.end(),
+              [&](const int &fromBody, const int &toBody) -> bool {
+                return _bodies[fromBody]->springs.size() > _bodies[toBody]->springs.size();
+              });
+
+    for (auto body : _bodies){
+      Vector3<N> base;
+      for (auto other: body->springs) {
+        base.add(other->pos);
+      }
+      int neighboursSize = body->springs.size();
+      if (neighboursSize > 0) {
+        base.multiplyScalar(1./neighboursSize);
+      }
+
+      int springLength = _settings.springLength;
+      Vector3<N> offset;
+      for (size_t i = 0; i < N; ++i) {
+        offset.coord[i] = springLength * (random.nextDouble() - 0.5);
+      }
+      body->pos.set(base)->add(offset);
+    }
+  }
   
   static LayoutSettings makeDefaultSettings() {
     LayoutSettings settings;
     return settings;
   }
+
 public:
   ForceLayout(Graph &g, LayoutSettings settings) :
     graph(g),
@@ -91,21 +209,38 @@ public:
   /**
    * Sets a position for body. If no such body exist, throws BodyNotFoundException.
    */
-  void setPosition(const std::size_t &bodyId, const Vector3 &position);
+  void setPosition(const std::size_t &bodyId, const Vector3<N> &position) {
+    auto body = getBody(bodyId);
+    if (!body) {
+      throw BodyNotFoundException();
+    }
+    body->pos.set(position);
+  }
 
   /**
    * Performs one iteration of force layout. Returns total movement performed
    * during that step.
    */
-  double step();
+  double step() {
+    accumulate();
+
+    double totalMovement = integrate();
+    return totalMovement;
+  }
   
   /**
    * Given a node id from a graph, returns a pointer to its body object or
    * null if no such node is found.
    */
-  Body *getBody(const std::size_t &nodeId);
+  Body<N> *getBody(const std::size_t &nodeId) {
+    auto search = _nodeIdToBody.find(nodeId);
+    if (search == _nodeIdToBody.end()) {
+      return nullptr;
+    }
+    return search->second;
+  }
   
-  QuadTree *getTree() { return &tree; }
+  QuadTree<N> *getTree() { return &tree; }
 };
 
 #endif
--- test/main.cc
+++ test/main.cc
@@ -5,66 +5,88 @@
 #include "nangraph.cc/graph.h"
 #include "quadtree.cc/quadtree.h"
 
-TEST_CASE("it can make a step", "[layout]" ) {
+TEST_CASE("it can make a step in", "[layout 3d]" ) {
   Graph graph;
   auto linkId = graph.addLink(1, 2);
 
-  ForceLayout layout(graph);
+  ForceLayout<3> layout(graph);
   auto movement = layout.step();
   REQUIRE(movement > 0);
 }
 
-TEST_CASE("it can set body position", "[layout]" ) {
+TEST_CASE("it can set body position", "[layout 3d]" ) {
   Graph graph;
   graph.addLink(1, 2);
 
-  ForceLayout layout(graph);
-  Vector3 position(0, 1, 0);
+  ForceLayout<3> layout(graph);
+  Vector3<3> position;
+  position.coord[0] = 0;
+  position.coord[1] = 1;
+  position.coord[2] = 0;
   layout.setPosition(1, position);
   auto body = layout.getBody(1);
   REQUIRE(body->pos == position);
 }
 
-TEST_CASE("it can return a body", "[layout]" ) {
+TEST_CASE("it can return a body", "[layout 3d]" ) {
   Graph graph;
   graph.addLink(1, 2);
 
-  ForceLayout layout(graph);
+  ForceLayout<3> layout(graph);
   layout.step();
 
-  Body *first = layout.getBody(1);
+  auto first = layout.getBody(1);
   REQUIRE(first != nullptr);
-  REQUIRE(first->pos.x != 0);
-  REQUIRE(first->pos.y != 0);
-  REQUIRE(first->pos.z != 0);
+  REQUIRE(first->pos.coord[0] != 0);
+  REQUIRE(first->pos.coord[1] != 0);
+  REQUIRE(first->pos.coord[2] != 0);
 
-  Body *second = layout.getBody(2);
+  auto second = layout.getBody(2);
   REQUIRE(second != nullptr);
-  REQUIRE(second->pos.x != 0);
-  REQUIRE(second->pos.y != 0);
-  REQUIRE(second->pos.z != 0);
+  REQUIRE(second->pos.coord[0] != 0);
+  REQUIRE(second->pos.coord[1] != 0);
+  REQUIRE(second->pos.coord[2] != 0);
 
-  Body *none = layout.getBody(42);
+  auto none = layout.getBody(42);
   REQUIRE(none == nullptr);
 }
 
-TEST_CASE("it can step with no links present", "[layout]" ) {
+TEST_CASE("it can step with no links present", "[layout 3d]" ) {
   Graph graph;
   graph.addNode(1);
   graph.addNode(2);
 
-  ForceLayout layout(graph);
+  ForceLayout<3> layout(graph);
   layout.step();
 
-  Body *first = layout.getBody(1);
+  auto first = layout.getBody(1);
   REQUIRE(first != nullptr);
-  REQUIRE(first->pos.x != 0);
-  REQUIRE(first->pos.y != 0);
-  REQUIRE(first->pos.z != 0);
+  REQUIRE(first->pos.coord[0] != 0);
+  REQUIRE(first->pos.coord[1] != 0);
+  REQUIRE(first->pos.coord[2] != 0);
 
-  Body *second = layout.getBody(2);
+  auto second = layout.getBody(2);
   REQUIRE(second != nullptr);
-  REQUIRE(second->pos.x != 0);
-  REQUIRE(second->pos.y != 0);
-  REQUIRE(second->pos.z != 0);
+  REQUIRE(second->pos.coord[0] != 0);
+  REQUIRE(second->pos.coord[1] != 0);
+  REQUIRE(second->pos.coord[2] != 0);
+}
+
+TEST_CASE("2d: it can step with no links present", "[layout 2d]" ) {
+  Graph graph;
+  graph.addNode(1);
+  graph.addNode(2);
+
+  ForceLayout<2> layout(graph);
+  layout.step();
+
+  auto first = layout.getBody(1);
+  REQUIRE(first != nullptr);
+  REQUIRE(first->pos.coord[0] != 0);
+  REQUIRE(first->pos.coord[1] != 0);
+
+  auto second = layout.getBody(2);
+  REQUIRE(second != nullptr);
+  REQUIRE(second->pos.coord[0] != 0);
+  REQUIRE(second->pos.coord[1] != 0);
 }
